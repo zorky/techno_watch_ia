661e003e8f9ea575dd5cbe4775cad8fa79bbb202
Olivier DUVAL <zorky00@gmail.com>
2025-09-13 16:14:27 +0200

    fix send_articles_email cli

--- send_articles_email.py
+++ send_articles_email.py
@@ -81,22 +81,29 @@ def send_watch_articles(params: EmailTemplateParams):
     )
 
 if __name__ == "__main__":
+    from models.emails import EmailTemplateParams
     articles = [
-    "L'IA révolutionne la médecine en 2025",
-    "Les dernières avancées en robotique industrielle",
-    "Comment l'énergie solaire transforme les villes",
-    "La cybersécurité face aux nouvelles menaces",
-    "Le cloud computing et la gestion des données",
-    "L'impact de la 5G sur les objets connectés",
-    "Les tendances du e-commerce en Europe",
-    "L'automatisation dans le secteur bancaire",
-    "L'évolution des véhicules électriques",
-    "L'intelligence artificielle dans l'éducation"
+        {'title': "L'IA révolutionne la médecine en 2025", 'link': 'https://domain.ntld', 'summary': 'résumé', 'scoring': '60 %'},
+        {'title': "Comment l'énergie solaire transforme les villes", 'link': 'https://domain2.ntld', 'summary': 'résumé', 'scoring': '45 %'},
+    # "Les dernières avancées en robotique industrielle",
+    # "Comment l'énergie solaire transforme les villes",
+    # "La cybersécurité face aux nouvelles menaces",
+    # "Le cloud computing et la gestion des données",
+    # "L'impact de la 5G sur les objets connectés",
+    # "Les tendances du e-commerce en Europe",
+    # "L'automatisation dans le secteur bancaire",
+    # "L'évolution des véhicules électriques",
+    # "L'intelligence artificielle dans l'éducation"
     ]
     current_date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     email_subject = f"[VEILLE] Revue de veille techno du {current_date}"
-    html_content = render_email_template(articles, "email_template.html.j2")
-    text_content = render_email_template(articles, "email_template.text.j2")
+    email = EmailTemplateParams(
+        articles=articles,
+        keywords=['ia', 'agent'],
+        threshold=0.5
+    )
+    html_content = render_email_template(email, "email_template.html.j2")
+    text_content = render_email_template(email, "email_template.text.j2")
     print(html_content)
     print(text_content)
     _send_email(
