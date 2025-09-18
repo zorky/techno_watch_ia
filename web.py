d355e6c49af2964585cc453537eb4e936d086418
Olivier DUVAL <zorky00@gmail.com>
2025-09-18 16:06:13 +0200

    +fix affichage date UTC en heure locale (Paris)

--- web.py
+++ web.py
@@ -15,6 +15,10 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="templates/web"), name="static")
 templates = Jinja2Templates(directory="templates/web")
 
+#
+# Filtres pour les templates jinja2 
+# champ | filtre
+# 
 def format_date(value):
     """filtre jinga2 pour formatage de la date en FR"""
     if isinstance(value, str):
@@ -22,8 +26,21 @@ def format_date(value):
         return date_obj.strftime("%d/%m/%Y")
     return value.strftime("%d/%m/%Y")
 
-# Ajoute le filtre à l'environnement Jinja2 de FastAPI
+def format_local_datetime(utc_datetime, tz: str = "Europe/Paris"):
+    """filtre de conversion UTC -> heure Paris"""
+    from datetime import datetime, timezone
+    import pytz
+    if not utc_datetime:
+        return "N/A"
+    # Convertit UTC → Europe/Paris
+    local_tz = pytz.timezone(tz)
+    local_datetime = utc_datetime.replace(tzinfo=timezone.utc).astimezone(local_tz)
+    return local_datetime.strftime('%d/%m/%Y à %H:%M')
+
+
+# Ajoute les filtres à l'environnement Jinja2 de FastAPI pour utilisation champ | filtre
 templates.env.filters["format_date"] = format_date
+templates.env.filters["format_local_datetime"] = format_local_datetime
 
 @app.get("/")
 def read_articles(request: Request, date: str = None):
