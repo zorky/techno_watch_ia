649254d6306bfe71ac03e4337dbee28ae7753a70
Olivier DUVAL <zorky00@gmail.com>
2025-09-18 15:01:18 +0200

    +Pydantic pour vérif liste articles résumés à insérer +bulk insert (bulk_insert_mappings) +création de la table Article par SQLAlchemy +fix placement entité Article avec Base = declarative_base() +fix placeement @event.listens_for(Article, 'before_update') +clean code

--- db/db.py
+++ db/db.py
@@ -1,25 +1,66 @@
 from sqlalchemy import create_engine, and_
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import event
 from contextlib import contextmanager
-import datetime
-import sqlite3
+# import sqlite3
 import os
 from dotenv import load_dotenv
-from models.article import Article, ArticleModel
+# from models.article import Article, ArticleModel
+from models.article import ArticleModel
+from datetime import datetime, timezone
 
 load_dotenv()
 DB_PATH = os.getenv("DB_PATH", "techno-watch.db")
 
+Base = declarative_base()
+#
+# Modèles
+# /!\ Après le declarative_base()
+#
+class Article(Base):
+    """Modèle entité BDD"""
+    __tablename__ = 'articles'
+    id = Column(Integer, primary_key=True)
+    dt_created = Column(DateTime, 
+                        server_default=func.now(),
+                        default=lambda: datetime.now(timezone.utc),
+                        nullable=False, 
+                        index=True)
+    dt_updated = Column(DateTime, 
+                        onupdate=lambda: datetime.now(timezone.utc),  # Remplace utcnow()
+                        default=lambda: datetime.now(timezone.utc),
+                        nullable=False)
+    title = Column(String, nullable=False)
+    link = Column(String, nullable=False)
+    summary = Column(Text, nullable=False)
+    score = Column(String, nullable=False)
+    published = Column(String, nullable=False, index=True)    
+
+# Evènement pour màj de dt_updated - /!\ après la déclaration du modèle
+@event.listens_for(Article, 'before_update')
+def update_timestamp(mapper, connection, target):    
+    target.dt_updated = datetime.now(timezone.utc)
+
 #
 # Configuration SQL Alchemy
 # 
-Base = declarative_base()
-engine = create_engine(f'sqlite:///{DB_PATH}')
-Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
+SQLALCHEMY_DB = f'sqlite:///{DB_PATH}'
+engine = create_engine(
+    SQLALCHEMY_DB,
+    connect_args={"check_same_thread": False},  # Nécessaire pour SQLite avec FastAPI et le multi-threads
+    echo=True  # Affiche les requêtes SQL (optionnel, pour le debug))
+)
+
+def init_db():
+    """Initialise la base de données SQLite."""
+    Base.metadata.create_all(engine)
+    print("Table 'articles' créée avec succès !")
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 #
 # Fonctions utilitaires DB
 # 
@@ -34,13 +75,21 @@ def get_db():
 
 def read_articles(date: str = None):
     """Lit les articles résumés qui ont été retenus pour la veille techno"""
-    from db.db import session
-    from models.article import Article    
-    if date:
-        articles = session.query(Article).filter(Article.published.like(f"%{date}%")).all()
-    else:
-        articles = session.query(Article).order_by(Article.published.desc()).all()
+    # from db.db import session
+    # from models.article import Article    
+    with get_db() as session:
+        if date:
+            articles = session.query(Article).filter(Article.published.like(f"%{date}%")).all()
+        else:
+            articles = session.query(Article).order_by(Article.published.desc()).all()
     return articles
+
+def _validate_and_get_articles_summaries(summaries):
+    validated_articles = [ArticleModel(**item) for item in summaries]
+    articles_data = [
+        article.model_dump() for article in validated_articles
+    ]  
+    return articles_data
 
 def save_to_db(summaries: list[dict]):
     """
@@ -54,72 +103,27 @@ def save_to_db(summaries: list[dict]):
         ValueError: Si la validation Pydantic échoue
         Exception: Pour les erreurs de base de données
     """
-    # from sqlalchemy.orm import sessionmaker
-    # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    # session = SessionLocal()
+    # from datetime import datetime
     with get_db() as session:        
-        try:
-            for item in summaries:
-                # existing_article = session.query(Article).filter_by(title=item["title"]).first()
-                existing_article = session.query(Article).filter(and_(
-                            Article.title == item["title"],
-                            Article.published == item["published"])).first()                
-                if not existing_article:
-                    article = Article(
-                        title=item["title"],
-                        link=item["link"],
-                        summary=item["summary"],
-                        score=item["score"],
-                        published=item["published"]
-                    )
-                    article.dt_updated = datetime.utcnow()
-                    session.add(article)
+        try:            
+            articles_data = _validate_and_get_articles_summaries(summaries)  
+            session.bulk_insert_mappings(Article, articles_data)
             session.commit()
-
-            # validation Pydantic
-            # """
-            # pydantic_core._pydantic_core.ValidationError: 1 validation error for ArticleModel
-            # date
-            # Field required [type=missing, input_value={'title': 'DjangoCon US\x...: '2025-09-07T22:00:00'}, input_type=dict]
-            #     For further information visit https://errors.pydantic.dev/2.11/v/missing
-
-            # """
-            # validated_articles = [ArticleModel(**item) for item in summaries]
-            # articles_data = [
-            #     article.model_dump() for article in validated_articles
-            # ]
-            # session.bulk_insert_mappings(Article, articles_data)
-            # session.commit()
-            ## session.bulk_insert_mappings(Article, [
-            ##     article.model_dump() for article in validated_articles
-            ## ])
-            # session.bulk_insert_mappings(Article, [{
-            #     "title": item["title"],
-            #     "link": item["link"],
-            #     "summary": item["summary"],
-            #     "score": item["score"],
-            #     "date": item["published"]
-            # } for item in summaries])
+            # for item in summaries:                                 
+            #     existing_article = session.query(Article).filter(and_(
+            #                 Article.title == item["title"],
+            #                 Article.published == item["published"])).first()                
+            #     if not existing_article:
+            #         article = Article(
+            #             title=item["title"],
+            #             link=item["link"],
+            #             summary=item["summary"],
+            #             score=item["score"],
+            #             published=item["published"]
+            #         )                                        
+            #         session.add(article)
             # session.commit()
         except Exception as e:
             session.rollback()
             raise e            
 
-def init_db():
-    """Initialise la base de données SQLite."""
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS articles (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            dt_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            dt_updated TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-            title TEXT NOT NULL,
-            link TEXT NOT NULL,
-            summary TEXT NOT NULL,
-            score TEXT NOT NULL,
-            published TEXT NOT NULL
-        )
-    """)
-    conn.commit()
-    conn.close()
