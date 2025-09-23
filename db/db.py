d8789119dc5d7046effb52779d3140e6c64246c7
.Olivier D. <zorky00@gmail.com>
2025-09-23 09:20:22 +0200

    Recherche FTS des résumés sur l'interface Web (#4)
    * Init recherche ajax fetch.
    Tailwind à la place du CSS custom
    Le rank est transformé en %
    
    * Triggers insert / update / delete sur Article pour articles_fts pour éviter un process manuel de batch insert à l'issu des insertions dans Article
    
    * fix recherche FTS pour renvoyer un dico de type {"title": "titre", ...}
    
    amélioration UI recherche
    
    factorisation par fragments HTML pour simplifier index.html
    
    * fix recherche sur terme vide
    
    ---------
    
    Co-authored-by: Olivier Duval <olivier.duval@univ-paris8.fr>

--- db/db.py
+++ db/db.py
@@ -52,20 +52,47 @@ class ArticleFTS:
     """Modèle pour la table FTS5 Full Text Search"""
     __tablename__ = 'articles_fts'
     @classmethod
-    def init_table(cls, engine):
+    def execute_statement(cls, conn, statement):
+        conn.execute(statement)
+        conn.commit()
+
+    @classmethod
+    def init_table(cls, engine):                
         """Crée la table FTS5 manuellement."""
-        with engine.connect() as conn:
-            conn.execute(text(f"""
+        with engine.connect() as conn:        
+            cls.execute_statement(conn, text(f"""
                 -- DROP TABLE IF EXISTS articles_fts;
                 CREATE VIRTUAL TABLE IF NOT EXISTS {cls.__tablename__} USING fts5(
+                    article_id,
                     title,
-                    content,
-                    published,
+                    content,                    
                     tokenize='unicode61',
                     prefix='2,3'
-                );
+                );                                               
+                """))
+            cls.execute_statement(conn, text(f"""
+            -- Trigger pour l'insertion
+                CREATE TRIGGER sync_article_fts AFTER INSERT ON {Article.__tablename__}
+                BEGIN
+                    INSERT INTO {cls.__tablename__}(article_id, title, content) VALUES (new.id, new.title, new.summary);
+                END;
             """))
-            conn.commit()
+            cls.execute_statement(conn, text(f"""
+            -- Trigger pour la mise à jour
+                CREATE TRIGGER sync_article_update AFTER UPDATE ON {Article.__tablename__}
+                BEGIN
+                    UPDATE {cls.__tablename__}
+                    SET title = new.title, content = new.summary
+                    WHERE article_id = new.id;
+                END;
+            """))
+            cls.execute_statement(conn, text(f"""
+            -- Trigger pour la suppression
+                CREATE TRIGGER sync_article_delete AFTER DELETE ON {Article.__tablename__}
+                BEGIN
+                    DELETE FROM {cls.__tablename__} WHERE article_id = old.id;
+                END;
+            """))            
 
     @classmethod
     def insert(cls, session, title, content, published):
@@ -94,27 +121,100 @@ class ArticleFTS:
     def search(cls, session, query, date_min=None, date_max=None, limit=10):
         """
         Recherche plein texte dans les articles avec filtre optionnel par date.
+        avec score basé sur rank().
 
         Args:
             session: Session SQLAlchemy
             query: Terme de recherche plein texte
             date_min: Date minimale (format YYYY-MM-DD, optionnel)
             date_max: Date maximale (format YYYY-MM-DD, optionnel)
+            limit: Nombre maximum de résultats
 
         Returns:
             Liste des articles correspondant aux critères
         """
+        # Les indices des 2 champs hightlights de la table articles_fts
+        IDX_TITLE_TABLE = 1
+        IDX_CONTENT_TABLE = 2        
+        
+        # Base SQL pour le CTE
         base_sql = f"""
-            SELECT 
-                rowid, 
-                title, 
-                content, 
-                published, 
-                rank, 
-                highlight({cls.__tablename__}, 0, '<b>', '</b>') AS title_highlight
-            FROM {cls.__tablename__}
+            WITH ranked AS (
+                SELECT                     
+                    rowid, 
+                    article_id,
+                    highlight({cls.__tablename__}, {IDX_TITLE_TABLE}, '<mark>', '</mark>') AS title,
+                    highlight({cls.__tablename__}, {IDX_CONTENT_TABLE}, '<mark>', '</mark>') AS content,                
+                    -rank AS score
+                FROM {cls.__tablename__}
+                WHERE {cls.__tablename__} MATCH :query
         """
-        where_clauses = [f"{cls.__tablename__} MATCH :query"]
+
+        # Ajout des filtres optionnels
+        where_clauses = []
+        params = {"query": query, "limit": limit}
+
+        if date_min:
+            where_clauses.append("a.published >= :date_min")
+            params["date_min"] = date_min
+        if date_max:
+            where_clauses.append("a.published <= :date_max")
+            params["date_max"] = date_max
+
+        base_sql += f"""
+            )
+            SELECT 
+                ranked.title as title, 
+                ranked.content as content,  
+                a.link as link,
+                a.published as published,                                                
+                ROUND(100.0 * ranked.score / (SELECT MAX(ranked.score) FROM ranked), 2) AS rank
+            FROM ranked
+            JOIN {Article.__tablename__} a on a.id = ranked.article_id
+        """
+        
+        if where_clauses:
+            base_sql += "    WHERE " + " AND ".join(where_clauses)
+            logger.info(f"where {base_sql}")
+
+        base_sql += f"""
+            ORDER BY rank DESC
+            LIMIT :limit
+        """
+
+        logger.info(f"SQL exécuté: {base_sql} avec {params}")
+        # results = session.execute(text(base_sql), params).fetchall()
+        results = session.execute(text(base_sql), params)
+        results_as_dict = results.mappings().all()
+        logger.info(f"results : {results_as_dict}")
+        return results_as_dict
+        # return results.fetchall()
+
+    @classmethod
+    def search_with_bm25(cls, session, query, date_min=None, date_max=None, limit=10):
+        """
+        Recherche plein texte dans les articles avec filtre optionnel par date,
+        avec score basé sur bm25() : on opérationnel
+        /!\ erreur :
+        sqlite3.OperationalError: unable to use function bm25 in the requested context
+        requête SQL générée :
+        WITH ranked AS ( SELECT rowid, highlight(articles_fts, 0, '<mark>', '</mark>') AS title, highlight(articles_fts, 1, '<mark>', '</mark>') AS content, published, bm25(articles_fts) AS score FROM articles_fts WHERE articles_fts MATCH ? ) SELECT rowid, title, content, published, ROUND( 100.0 * ( (SELECT MAX(score) FROM ranked) - score ) / NULLIF((SELECT MAX(score) FROM ranked) - (SELECT MIN(score) FROM ranked), 0), 2 ) AS rank FROM ranked ORDER BY rank DESC LIMIT ?
+        """
+
+        base_sql = f"""
+            WITH ranked AS (
+                SELECT 
+                    rowid, 
+                    highlight({cls.__tablename__}, 0, '<mark>', '</mark>') AS title,
+                    highlight({cls.__tablename__}, 1, '<mark>', '</mark>') AS content,
+                    published,
+                    bm25({cls.__tablename__}) AS score
+                FROM {cls.__tablename__}
+                WHERE {cls.__tablename__} MATCH :query
+        """
+
+        # Filtres optionnels
+        where_clauses = []
         params = {"query": query, "limit": limit}
 
         if date_min:
@@ -125,9 +225,25 @@ class ArticleFTS:
             params["date_max"] = date_max
 
         if where_clauses:
-            base_sql += " WHERE " + " AND ".join(where_clauses)
+            base_sql += " AND " + " AND ".join(where_clauses)
 
-        base_sql += " ORDER BY rank DESC LIMIT :limit"
+        # Normalisation : plus le score est faible, plus la pertinence est forte
+        base_sql += """
+            )
+            SELECT 
+                rowid, 
+                title, 
+                content, 
+                published,
+                ROUND(
+                    100.0 * ( (SELECT MAX(score) FROM ranked) - score ) 
+                    / NULLIF((SELECT MAX(score) FROM ranked) - (SELECT MIN(score) FROM ranked), 0), 
+                    2
+                ) AS rank
+            FROM ranked
+            ORDER BY rank DESC
+            LIMIT :limit
+        """
 
         logger.debug(f"SQL exécuté: {base_sql} avec {params}")
         return session.execute(text(base_sql), params).fetchall()
@@ -305,7 +421,7 @@ def save_to_db(summaries: list[dict]):
                 session.bulk_insert_mappings(Article, articles_data)
                 session.commit()      
 
-                save_to_fts(new_articles)
+                # save_to_fts(new_articles)
         except Exception as e:
             session.rollback()
             raise e            
