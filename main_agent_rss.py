b11eb256b65a28135713786cd56657489c5da9e8
.Olivier D. <zorky00@gmail.com>
2025-09-30 14:14:29 +0200

    feat: ajout source pour la veille techno : Bluesky (#7)
    * feat: ajout source pour la veille techno : Bluesky
    
    * ajout pre-commit
    
    * ajout comptes sources BlueSky pour myreddit.json (à renommer pour Reddit ET Bluesky)
    
    * obention des posts de comptes Bluesky
    package atproto
    
    * réactivation RSS et Reddit
    
    * --log rss published_parsed
    
    ---------
    
    Co-authored-by: Olivier Duval <olivier.duval@univ-paris8.fr>

--- main_agent_rss.py
+++ main_agent_rss.py
@@ -491,10 +491,12 @@ def register_fetchers():
     from services.factory_fetcher import FetcherFactory
     from services.reedit_fetcher import RedditFetcher
     from services.rss_fetcher import RSSFetcher
+    from services.bluesky_fetcher import BlueskyFetcher
     from services.models import SourceType
 
     FetcherFactory.register_fetcher(SourceType.RSS, RSSFetcher)
     FetcherFactory.register_fetcher(SourceType.REDDIT, RedditFetcher)
+    FetcherFactory.register_fetcher(SourceType.BLUESKY, BlueskyFetcher)
 
 
 def unified_fetch_node(state: UnifiedState) -> UnifiedState:
@@ -502,6 +504,9 @@ def unified_fetch_node(state: UnifiedState) -> UnifiedState:
 
     REDDIT_CLIENT_ID = os.getenv('REDDIT_CLIENT_ID', None)
     REDDIT_CLIENT_SECRET = os.getenv('REDDIT_CLIENT_SECRET', None)
+
+    BLUESKY_HANDLE = os.getenv("BLUESKY_HANDLE", "your_bluesky_handle.bsky.social")
+    BLUESKY_PASSWORD = os.getenv("BLUESKY_PASSWORD", "app_password")
 
     all_articles = []
 
@@ -513,6 +518,11 @@ def unified_fetch_node(state: UnifiedState) -> UnifiedState:
             client_id=REDDIT_CLIENT_ID,
             client_secret=REDDIT_CLIENT_SECRET,
             user_agent="TechnoWatch 1.0"
+        ),
+        SourceType.BLUESKY: FetcherFactory.create_fetcher(
+            SourceType.BLUESKY,
+            handle=BLUESKY_HANDLE,
+            password=BLUESKY_PASSWORD
         )
     }
 
@@ -648,7 +658,6 @@ def make_graph():
     graph.add_edge("savedbsummaries", "sendsummaries")
 
     return graph.compile()
-
 
 def get_rss_urls():
     """
