09fda5f1226fe79ab823b8cd7d1362f377a04c80
Olivier Duval <olivier.duval@univ-paris8.fr>
2025-10-27 13:32:31 +0100

    fix db.db => app.db save_nodes déplacement ollama.yml => docker

--- app/nodes/save_nodes.py
+++ app/nodes/save_nodes.py
@@ -3,10 +3,9 @@ logging.basicConfig(level=logging.INFO)
 from colorama import Fore
 from app.core.logger import logger
 from app.models.states import RSSState
+from app.db import save_to_db
 
-def save_articles_node(state: RSSState) -> RSSState:
-    from db.db import save_to_db
-
+def save_articles_node(state: RSSState) -> RSSState:    
     logger.info(Fore.LIGHTWHITE_EX + "Sauvegarde des articles résumés en DB")
     if len(state.summaries) > 0:
         save_to_db(state.summaries)
